993aab768e26e8a2b2b17f765a96fa2c4f3ea294
rafaelmaia <rafaelmfcontato@protonmail.com>
2025-11-20 16:45:12 -0300

    add home page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <h1>
+            Meu mozin, levantei um servidor online para dizer que eu te amo
+            muito! 💖💖💖💖💖💖
+        </h1>
+    );
+}
+
+export default Home;
