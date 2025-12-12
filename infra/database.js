1d5e2db281efe3d6213215a2d73df6c898c8eaee
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2025-12-12 14:29:45 +0000

    adds  `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,20 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -10,25 +24,20 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  try{
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch  (error) {
-    console.error(error);
-    throw error;
-  } finally {
-     await client.end();
-  }
+  await client.connect();
+  return client;
+}
+
+export default {
+  query,
+  getNewClient,
 };
 
-export default { query };
-
-function getSSLValues(){
+function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
-      ca: process.env.POSTGRES_CA
-    }
-  };
-  return process.env.NODE_ENV === 'development' ? false : true;
-} ;
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "production" ? true : false;
+}
