a0fced4eef3138c6b6c11db53296a29ff527ad03
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2026-02-19 15:50:07 +0000

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import database from "infra/database";
+import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function findOneByUsername(username) {
@@ -32,11 +33,12 @@ async function findOneByUsername(username) {
   }
 }
 
-async function create(userInputValue) {
-  await validadeUniqueEmail(userInputValue.email);
-  await validateUniqueUsername(userInputValue.username);
+async function create(userInputValues) {
+  await validadeUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
-  const newUser = await runInsertQuery(userInputValue);
+  const newUser = await runInsertQuery(userInputValues);
   return newUser;
 
   async function validadeUniqueEmail(email) {
@@ -81,7 +83,12 @@ async function create(userInputValue) {
     }
   }
 
-  async function runInsertQuery(userInputValue) {
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
+  }
+
+  async function runInsertQuery(userInputValues) {
     const results = await database.query({
       text: `
       INSERT INTO 
@@ -92,9 +99,9 @@ async function create(userInputValue) {
       *
     ;`,
       values: [
-        userInputValue.username,
-        userInputValue.email,
-        userInputValue.password,
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
       ],
     });
     return results.rows[0];
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -25,11 +27,12 @@ describe("POST /api/v1/users", () => {
       expect(response.status).toBe(201);
 
       const responseBody = await response.json();
+
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: "rafaelMaia",
         email: "rafael@example.com",
-        password: "password123",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -37,6 +40,20 @@ describe("POST /api/v1/users", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const userInDatabase = await user.findOneByUsername("rafaelMaia");
+      const correctPasswordMatch = await password.compare(
+        "password123",
+        userInDatabase.password,
+      );
+
+      const incorrectPasswordMatch = await password.compare(
+        "senhaErrada",
+        userInDatabase.password,
+      );
+
+      expect(correctPasswordMatch).toBe(true);
+      expect(incorrectPasswordMatch).toBe(false);
     });
 
     test("With duplicated 'email'", async () => {
