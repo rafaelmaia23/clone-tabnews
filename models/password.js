e1e88ea51ed24cfcac00e868871c8442e1a97ea9
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2026-02-19 16:30:27 +0000

    refactor: add `pepper` in `password.hash` and `password.compare`

--- models/password.js
+++ models/password.js
@@ -1,16 +1,22 @@
 import bcryptjs from "bcryptjs";
 
 async function hash(password) {
+  const pepper = getPepper();
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  return await bcryptjs.hash(password + pepper, rounds);
 }
 
 function getNumberOfRounds() {
   return process.env.NODE_ENV === "production" ? 14 : 1;
 }
 
+function getPepper() {
+  return process.env.PASSWORD_PEPPER || "password_pepper_default_value";
+}
+
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  const pepper = getPepper();
+  return await bcryptjs.compare(providedPassword + pepper, storedPassword);
 }
 
 const password = {
