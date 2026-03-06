0e98f42ffc3c6f59e96f21a6d484f9616262e2c0
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2026-03-06 15:09:30 +0000

    feat: implement session creation

--- infra/controller.js
+++ infra/controller.js
@@ -3,15 +3,19 @@ import {
   MethodNotAllowedError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 } from "infra/errors.js";
 
 function onErrorHandler(err, req, res) {
-  if (err instanceof ValidationError || err instanceof NotFoundError) {
+  if (
+    err instanceof ValidationError ||
+    err instanceof NotFoundError ||
+    err instanceof UnauthorizedError
+  ) {
     return res.status(err.statusCode).json(err);
   }
 
   const publicErrorObject = new InternalServerError({
-    statusCode: err.statusCode,
     cause: err,
   });
 
