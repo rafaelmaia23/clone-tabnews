8486515b1d5cdc454db7a8c0d97ed809035e76ad
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2026-04-14 20:49:19 +0000

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,6 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "rafaelMaia",
         email: "rafael@example.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
