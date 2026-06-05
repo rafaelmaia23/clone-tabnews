e6826778e58e415ca712146cd5fe02e85fca2181
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2026-06-05 15:18:42 +0000

    feat: add `sameSite=Lax` to `controller.setSessionCookie()`

--- infra/controller.js
+++ infra/controller.js
@@ -46,6 +46,7 @@ function setSessionCookie(sessionToken, res) {
     maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
+    sameSite: "lax",
   });
 
   res.setHeader("set-cookie", setCookie);
