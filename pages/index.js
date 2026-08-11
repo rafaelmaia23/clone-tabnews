9e5a2e0aa2358beaa3997ec1df57e74fef835043
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2026-08-11 16:09:55 +0000

    feat: use `DefaultLayout` on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import DefaultLayout from "interface/DefaultLayout";
+
 function Home() {
-  return <h1>Site em construção =D</h1>;
+  return (
+    <DefaultLayout
+      metadata={{
+        description: "Clone do TabNews, um site de notícias e discussões.",
+      }}
+    >
+      <h1>Clone do TabNews</h1>
+      <h1>Site em construção =D</h1>
+    </DefaultLayout>
+  );
 }
 
 export default Home;
